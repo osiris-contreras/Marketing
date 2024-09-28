869b60caf4f8845879692b7520ec4b35ae3072b5
jfelipeo <jfelipe_o@hotmail.com>
2024-09-27 21:44:25 -0500

    Subir inicio exploracion y preprocesamiento

---
+++ b_exploracion_limpieza.py
@@ -0,0 +1,163 @@
+import numpy as np
+import pandas as pd
+import sqlite3 as sql
+import plotly.graph_objs as go ### para gráficos
+import plotly.express as px
+import a_funciones as fn
+## pip install mlxtend
+from mlxtend.preprocessing import TransactionEncoder
+
+#pip install --upgrade nbformat
+###pip install  pysqlite3
+
+
+
+###### para ejecutar sql y conectarse a bd ###
+conn=sql.connect('data\\db_movies2') ### crear cuando no existe el nombre de cd  y para conectarse cuando sí existe.
+cur=conn.cursor() ###para funciones que ejecutan sql en base de datos
+
+##conn.close() ### cerrar conexion base de datos
+
+##Verificar tablas que hay en la base de datos
+cur.execute("SELECT name FROM sqlite_master where type='table' ")
+cur.fetchall()
+
+##### consultar trayendo para pandas ###
+ratings=pd.read_sql("select * from ratings", conn)
+movies=pd.read_sql("select * from movies", conn)
+
+### tratamiento para separar generos en columnas
+genres=movies['genres'].str.split('|')
+te = TransactionEncoder()
+genres = te.fit_transform(genres)
+genres = pd.DataFrame(genres, columns = te.columns_)
+
+### unir tablas
+movies_tratada = pd.concat([movies, genres], axis=1)
+movies_tratada = movies_tratada.drop('genres', axis=1)
+
+
+##### EXPLORACIÓN INICIAL
+movies_tratada.info()
+movies_tratada.head()
+movies_tratada.duplicated().sum()
+
+# ver duplicados por titulo 
+duplicados = movies_tratada[movies_tratada.duplicated(subset=['title'], keep=False)]
+duplicados.sort_values(by='title')
+
+### para llevar un data frame de pandas a SQL
+completo = movies_tratada
+completo.to_sql('movies',conn,index=False, if_exists='replace')
+
+##### CANTIDAD DE CALIFICACIONES POR PELICULA
+ratings.info()
+ratings.head()
+ratings.duplicated().sum()
+
+
+##### DESCRIPCIÓN DE BASE RATINGS
+
+### calcular distribución de calificaciones
+cr=pd.read_sql(""" select 
+                          "rating", 
+                          count(*) as conteo 
+                          from ratings
+                          group by "rating"
+                          order by conteo desc""", conn)
+
+data  = go.Bar( x=cr.rating,y=cr.conteo, text=cr.conteo, textposition="outside")
+Layout=go.Layout(title="Count of ratings",xaxis={'title':'Rating'},yaxis={'title':'Count'})
+go.Figure(data,Layout)
+
+
+
+### CANTIDAD DE PELICULAS CALIFICADAS POR USUARIO
+rating_users=pd.read_sql(''' select "userId",
+                         count(*) as cnt_rat
+                         from ratings
+                         group by "userId"
+                         order by cnt_rat asc
+                         ''',conn )
+
+fig  = px.histogram(rating_users, x= 'cnt_rat', title= 'Histrograma de # calificaciones por usario')
+fig.show() 
+
+rating_users.describe()
+### la mayoria de usarios tiene pocas Peliculas calificadas, pero los que más tienen muchos
+
+
+
+#### EXCLUIR USUARIOS CON MENOS DE 50 Y MAS DE 450 PELICULAS CALIFICADAS
+rating_users2=pd.read_sql(''' select "userId",
+                         count(*) as cnt_rat
+                         from ratings
+                         group by "userId"
+                         having cnt_rat >=10 and cnt_rat <=450
+                         order by cnt_rat asc
+                         ''',conn )
+
+rating_users2.describe()
+
+fig  = px.histogram(rating_users2, x= 'cnt_rat', title= 'Hist frecuencia de numero de calificaciones por usario')
+fig.show() 
+
+
+
+#### CANTIDAD DE CALIFICACIONES POR PELICULA
+rating_movies=pd.read_sql(''' select movieId ,
+                         count(*) as cnt_rat
+                         from ratings
+                         group by "movieId"
+                         order by cnt_rat desc
+                         ''',conn )
+
+fig  = px.histogram(rating_movies, x= 'cnt_rat', title= 'Hist frecuencia de numero de calificaciones para cada pelicula')
+fig.show() 
+
+rating_movies.describe()
+
+
+#### EXCLUIR PELICULAS CON MENOS DE 10 CALIFICACIONES
+rating_movies2=pd.read_sql(''' select movieId ,
+                         count(*) as cnt_rat
+                         from ratings
+                         group by "movieId"
+                         having cnt_rat>=10
+                         order by cnt_rat desc
+                         ''',conn )
+
+rating_movies2.describe()
+
+fig  = px.histogram(rating_movies2, x= 'cnt_rat', title= 'Hist frecuencia de numero de calificaciones para cada pelicula')
+fig.show() 
+
+
+
+#### EJECUCIÓN DEL PREPROCESAMIENTO - SQL
+fn.ejecutar_sql('preprocesamientos.sql', cur)
+## Verificar tablas que hay en la base de datos
+cur.execute("select name from sqlite_master where type='table' ")
+cur.fetchall()
+
+ratings_final=pd.read_sql("select * from ratings", conn)
+movies_final=pd.read_sql("select * from movies", conn)
+full_ratings=pd.read_sql('select * from full_ratings',conn)
+
+### verficar tamaño de tablas con filtros ####
+
+####movies
+pd.read_sql('select count(*) from movies', conn)
+pd.read_sql('select count(*) from movies_final', conn)
+
+##ratings
+pd.read_sql('select count(*) from ratings', conn)
+pd.read_sql('select count(*) from ratings_final', conn)
+
+## 3 tablas cruzadas ###
+pd.read_sql('select count(*) from full_ratings', conn)
+
+ratings=pd.read_sql('select * from full_ratings',conn)
+ratings.duplicated().sum() ## al cruzar tablas a veces se duplican registros
+ratings.info()
+ratings.head(10)
---
+++ preprocesamientos.sql
@@ -0,0 +1,71 @@
+----procesamientos---
+
+---crear tabla con usuarios de peliculas que han sido ranquiadas entre 10 y 450
+
+drop table if exists usuarios_sel;
+
+create table usuarios_sel as 
+
+select "userId" as user_id, count(*) as cnt_rat
+from ratings
+group by "userId"
+having cnt_rat >10 and cnt_rat <= 450
+order by cnt_rat desc ;
+
+
+
+---crear tabla con peliculas que han sido ranquiadas mayor a 10
+drop table if exists movies_sel;
+
+
+
+create table movies_sel as select movieId,
+                         count(*) as cnt_rat
+                         from ratings
+                         group by movieId
+                         having cnt_rat>=10
+                         order by cnt_rat desc ;
+
+
+-------crear tablas filtradas de peliculas y calificaciones ----
+
+drop table if exists ratings_final;
+
+create table ratings_final as
+select a."userId"as user_id,
+a.movieId as movieId,
+a."rating" as rating
+from ratings a 
+inner join movies_sel b
+on a.movieId =b.movieId
+inner join usuarios_sel c
+on a."userId" =c.user_id;
+
+drop table if exists movies_final;
+
+create table movies_final as
+select a.movieId as movieId,
+a."title"  as title,
+a."genres" as genres
+from movies a
+inner join movies_sel c
+on a.movieId= c.movieId;
+
+
+---crear tabla completa ----
+
+drop table if exists full_ratings;
+
+create table full_ratings as 
+select 
+    a.user_id,
+    a.movieId,
+    a.rating,
+    b.title,
+    b.genres
+from 
+    ratings_final a 
+inner join 
+    movies_final b 
+on 
+    a.movieId = b.movieId;
